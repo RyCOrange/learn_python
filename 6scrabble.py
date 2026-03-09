d6ddbae24ed0a342d2f1bbfa939104ca74746ccc
RyCOrange <hotsplat2@gmail.com>
2026-03-09 07:52:38 -0500

    Short scrabble game

--- 6scrabble.py
+++ 6scrabble.py
@@ -1,27 +1,26 @@
 # A short 2-player game based on scrabble
 # 2 players put in their words, the player with the highest-point word wins
 
-# Get player 1 input
-# Get player 2 input
-# Pull each letter points from points array
-# Sum each player points
-# Check player points against each other, check for win or tie
-# Print winner
-
+# Calculates player 1 word value
 def points_calc1(word1):
     points = 0
-    lets = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+    lets = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]    
+    # Pull each letter points from points array
     for letter in word1:
+        # Sum player points
         if letter.isupper():
             points = lets[ord(letter) - ord('A')] + points
         elif letter.islower():
             points = lets[ord(letter) - ord('a')] + points
     return(points)
 
+# Calculates player 2 word value
 def points_calc2(word2):
     points = 0
     lets = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+    # Pull each letter points from points array
     for letter in word2:
+        # Sum player points
         if letter.isupper():
             points = lets[ord(letter) - ord('A')] + points
         elif letter.islower():
@@ -29,8 +28,9 @@ def points_calc2(word2):
     return(points)
 
 def main():
-    word1 = input("Player 1 word: ")
-    word2 = input("Player 2 word: ")
+    word1 = input("Player 1 word: ") # Get player 1 input
+    word2 = input("Player 2 word: ") # Get player 2 input
+    # Check player points against each other, check for win or tie, prints winner or tie
     if points_calc1(word1) > points_calc2(word2):
         print(f"Player 1 wins with {word1}!")
     elif points_calc1(word1) < points_calc2(word2):
